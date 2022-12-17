57fc3644cd6b7abf018d468488fd7d93871d2803
spuun <kek@spuun.art>
2022-12-17 22:57:27 +0700

    Looks better - Fonts - Responsiveness - JS controlling ^

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-var typed = new Typed("#adj", {
-  // Waits 1000ms after typing "First"
-  strings: ["Simple", "Efficient", "Fast", "Secure"],
-});
+var adjArray = ["Efficient", "Fast", "Secure", "Simple"],
+  typed = new Typed("#adj", {
+    strings: adjArray,
+    typeSpeed: 40,
+    showCursor: false,
+    onComplete: (self) => {
+      Object.assign(document.getElementById("adj").style, {
+        color: "gold",
+      });
+      Object.assign(document.getElementById("title").style, {
+        "font-size": "300%",
+      });
+      Object.assign(document.getElementById("main-content").style, {
+        "max-height": "15em",
+        padding: "1em",
+        "padding-bottom": "2em",
+      });
+      setTimeout(() => {
+        Object.assign(document.getElementById("rise").style, {
+          color: "red",
+        });
+      }, 1500);
+    },
+  });
