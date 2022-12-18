67a8a4b8171007e703f4a5dfbaef2714c1c7271f
spuun <kek@spuun.art>
2022-12-19 03:42:32 +0700

    - Add another line - More responsiveness, we're using clamp on text size other than slogan - Title size lowered to prevent clipping on larger screens - transition added for content paddings to allow for smooth unfolding - JS for scrolling to scroll to currently highlighted elements

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function scrollHere(id) {
+  document
+    .getElementById(id)
+    .scrollIntoView({ alignToTop: true, behavior: "smooth" });
+}
+
 var adjArray = ["Efficient", "Fast", "Secure", "Simple"],
   typed = new Typed("#adj", {
     strings: adjArray,
@@ -8,17 +14,46 @@ var adjArray = ["Efficient", "Fast", "Secure", "Simple"],
         color: "gold",
       });
       Object.assign(document.getElementById("title").style, {
-        "font-size": "300%",
-      });
-      Object.assign(document.getElementById("main-content").style, {
-        "max-height": "15em",
-        padding: "1em",
-        "padding-bottom": "2em",
+        "font-size": "250%",
       });
       setTimeout(() => {
-        Object.assign(document.getElementById("rise").style, {
-          color: "red",
+        Object.assign(document.getElementById("main-content").style, {
+          "max-height": "200vh",
         });
+        setTimeout(() => {
+          Object.assign(document.getElementById("main-content").style, {
+            padding: "1em",
+            "padding-bottom": "2em",
+          });
+          Object.assign(document.getElementById("rise").style, {
+            color: "red",
+          });
+          scrollHere("rise");
+          setTimeout(() => {
+            setTimeout(() => {
+              // NOTE: First "this."
+              scrollHere("look-like");
+              setTimeout(() => {
+                // NOTE: Then "kinda..."
+                setTimeout(() => {
+                  // NOTE: Then "similar"
+                  scrollHere("look-like");
+                }, 1500);
+              }, 3500);
+            }, 3000);
+
+            typed = new Typed("#look-like", {
+              strings: [
+                "and they look like...^1000 this.^2000",
+                "and they look like... this.<br/>" +
+                  " Compared to real stuff,^500 they look kinda^1000." +
+                  "^1000.^1000. <b>similar</b>,^1000 right?^1000",
+              ],
+              smartBackspace: true,
+              showCursor: false,
+            });
+          }, 1500);
+        }, 1500);
       }, 1500);
     },
   });
