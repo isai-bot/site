d5cb06e07a812fb601b9794cdd0970bb8028c252
spuun <kek@spuun.art>
2022-12-22 22:30:46 +0700

    Add image samples To-do: - Swap them out for more efficient formats. - Choose better images? Dunno, will look into legal implications.

--- index.js
+++ index.js
@@ -4,57 +4,103 @@ function scrollHere(id) {
     .scrollIntoView({ alignToTop: true, behavior: "smooth" });
 }
 
-var adjArray = ["Efficient", "Fast", "Secure", "Simple"],
-  typed = new Typed("#adj", {
-    strings: adjArray,
-    typeSpeed: 40,
-    showCursor: false,
-    onComplete: (self) => {
-      Object.assign(document.getElementById("adj").style, {
-        color: "gold",
-      });
-      Object.assign(document.getElementById("title").style, {
-        "font-size": "250%",
-      });
-      scrollHere("q");
-      setTimeout(() => {
-        Object.assign(document.getElementById("main-content").style, {
-          "max-height": "200vh",
+window.addEventListener("load", () => {
+  var adjArray = ["Efficient", "Fast", "Secure", "Simple"],
+    typed = new Typed("#adj", {
+      strings: adjArray,
+      typeSpeed: 20,
+      showCursor: false,
+      onComplete: (self) => {
+        Object.assign(document.getElementById("adj").style, {
+          color: "gold",
         });
+        Object.assign(document.getElementById("title").style, {
+          "font-size": "250%",
+        });
+        scrollHere("q");
         setTimeout(() => {
           Object.assign(document.getElementById("main-content").style, {
-            padding: "1em",
-            "padding-bottom": "2em",
+            "max-height": "200vh",
           });
-          Object.assign(document.getElementById("rise").style, {
-            color: "red",
-          });
-          scrollHere("rise");
           setTimeout(() => {
-            setTimeout(() => {
-              // NOTE: First "this."
-              scrollHere("look-like");
-              setTimeout(() => {
-                // NOTE: Then "kinda..."
-                setTimeout(() => {
-                  // NOTE: Then "similar"
-                  scrollHere("look-like");
-                }, 1500);
-              }, 3500);
-            }, 3000);
-
-            typed = new Typed("#look-like", {
-              strings: [
-                "and they look like...^1000 this.^2000",
-                "and they look like... this.<br/>" +
-                  " Compared to real stuff,^500 they look kinda^1000." +
-                  "^1000.^1000. <b>similar</b>,^1000 right?^1000",
-              ],
-              smartBackspace: true,
-              showCursor: false,
+            scrollHere("head-quote");
+            Object.assign(document.getElementById("main-content").style, {
+              padding: "1em",
+              "padding-bottom": "2em",
             });
+            Object.assign(document.getElementById("rise").style, {
+              color: "red",
+            });
+            setTimeout(() => {
+              setTimeout(() => {
+                scrollHere("look-like");
+                anime({
+                  targets: "#fsample",
+                  width: ["0vw", "35vw"],
+                  opacity: ["0", "1"],
+                  easing: "easeInOutExpo",
+                });
+                setTimeout(() => {
+                  scrollHere("sample-foot");
+                  setTimeout(() => {
+                    anime({
+                      targets: "#ssample",
+                      left: ["-100%", "0%"],
+                      opacity: "100%",
+                      easing: "easeInOutExpo",
+                    });
+                    setTimeout(() => {
+                      scrollHere("sample-foot");
+                      setTimeout(() => {
+                        anime({
+                          targets: "#ssample",
+                          left: ["0%", "-102.8%"],
+                          filter: "invert(1)",
+                          easing: "easeInOutExpo",
+                        });
+                        anime({
+                          targets: "#ssample p",
+                          opacity: "0",
+                          easing: "easeInOutExpo",
+                        });
+                        anime({
+                          targets: "#fsample",
+                          opacity: ["100%", "70%"],
+                          easing: "easeInOutExpo",
+                        });
+                        setTimeout(() => {
+                          anime({
+                            targets: "#fsample",
+                            opacity: ["70%", "50%"],
+                            easing: "easeInOutExpo",
+                          });
+                          setTimeout(() => {
+                            anime({
+                              targets: "#fsample",
+                              opacity: ["50%", "100%"],
+                              easing: "easeInOutExpo",
+                            });
+                          }, 1000);
+                        }, 1500);
+                      }, 2500);
+                    }, 4500);
+                  }, 1200);
+                }, 1550);
+              }, 1500);
+
+              typed = new Typed("#look-like", {
+                strings: [
+                  "and they look like...^1000 this.^2000",
+                  "Compared to real stuff,^500 they look kinda^1000." +
+                    "^1000.^1000. <strike>better</strike> <b>similar</b>,^1000 right?^1000",
+                  "So the question is,^1000 how do we <b>differentiate</b> them?",
+                ],
+                smartBackspace: true,
+                showCursor: false,
+              });
+            }, 1500);
           }, 1500);
         }, 1500);
-      }, 1500);
-    },
-  });
+      },
+    });
+});
